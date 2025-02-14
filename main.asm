23223d016bd84bcc4e6d32a38528a3db5c16e14b
car23979 <125841463+car23979@users.noreply.github.com>
2025-02-14 16:17:05 -0600

    Libreria
    Se incluyo la libreria

--- main.asm
+++ main.asm
@@ -6,4 +6,13 @@
 * Descripción: Implementación de un Timer0 y dos botones
 */
 // Encabezado
+.include "M328PDEF.inc"
 
+.cseg
+.org	0x0000
+
+// Configuración de pila
+LDI		R16, LOW(RAMEND)
+OUT		SPL, R16
+LDI		R16, HIGH(RAMEND)
+OUT		SPL, R16
